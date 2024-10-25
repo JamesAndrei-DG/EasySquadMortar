05e77af3921722402b6b1463393b9975513107aa
JamesAndrei-DG <jamesandrei.dg@gmail.com>
2024-10-25 23:54:52 +0800

    Added distance function

--- main.py
+++ main.py
@@ -24,10 +24,8 @@ def main():
 
     fire_solution = FireSolution()
     while True:
-        # Velocity is 110m/s for mortars
 
-        fire_solution.get_bearing()
-        fire_solution.get_radian()
+        fire_solution.get_distance()
         if debug:
             global frame_count
             global fps_display_interval
@@ -41,10 +39,12 @@ def main():
 
 class FireSolution:
     # initialization
+    velocity_squared = 12100  # velocity squared of mortar we should add more for other input
+    gravity = 0.1020408  # for multiplication
     screen_resolution = (1920, 1080)
     bearing_screen_coordinates = {"top": 1050, "left": 940, "width": 41, "height": 16}
     radian_screen_coordinates = {"top": int(screen_resolution[1] / 2 - 100 / 2),
-                                 "left": 530, "width": 60, "height": 100}
+                                 "left": 530, "width": 60, "height": 110}
 
     # value[1] for the second number only if detected
     # value[x][0] list of coordinates starting from index[0] = top left and proceeding clockwise
@@ -65,12 +65,25 @@ class FireSolution:
 
         if debug:
             print("Getting Radian")
+        # Variables for radians
         self.radian_monochrome = None
+        self.radian_results = None
+        self.box_height = [0, 0]
+        self.box_position = [0, 0]
+        self.box_difference = None
+        self.pixel_per_mills = 5
+
         while True:
             self.radian = self.get_radian()
             if self.radian:
                 print(f"Radian is: {self.radian}")
                 break
+
+    def get_distance(self):
+        bearing = self.get_bearing()
+        radian = self.get_radian() * 0.001
+        distance = self.velocity_squared * numpy.sin(radian * 2) * self.gravity
+        print(f"distance is: {distance}m")
 
     def get_radian_ocr_results(self):  # this will return easyocr results for radian
         # we should follow get_bearing_ocr_results for consistency
@@ -97,49 +110,60 @@ class FireSolution:
 
     def get_radian(self):
 
-        value = self.get_radian_ocr_results()
-        # check if value is divisible by 50 as it is height > 25 usually 37
-        # first check if the height of the first point (top left) is less than half of the total height of screen coordinate
+        self.radian_results = self.get_radian_ocr_results()
 
-        # create input where it will find the number of pixel space between 2 milliradian (can i use opencv again?) for my screen 6 pixels between
+        if self.radian_results:
+            for index, number in enumerate(self.radian_results):
+                # compute box height
+                top_left_y_axis = number[0][0][1]
+                bot_left_y_axis = number[0][3][1]
+                screen_edge = self.radian_screen_coordinates["height"]
+                tolerance = 1  # number tolerance for pixel
 
-        # create algo that will surely check the box height
-        # boxheight = [35,25]
+                # check if the box is on edge or not
+                if top_left_y_axis != 0 and bot_left_y_axis != screen_edge:
+                    self.box_height[index] = int(bot_left_y_axis - top_left_y_axis)
 
-        for item in value:
-            if item[0][0][1] == 0:  # for numbers that are on the top edge
-                big = False
+                # get box position
+                if self.box_height[index] != 0:
+                    self.box_position[index] = bot_left_y_axis - (self.box_height[index] * 0.5)
 
-            number = int(item[1])
-            # item[0][2]-item[0][0]
+                if index == 1:
+                    self.box_difference = self.box_position[1] - self.box_position[0]
+                    self.pixel_per_mills = self.box_difference * 0.1
 
-            if number % 50 == 0:
-                big = True
-                # early return
-        if debug:
-            if value:
-                boxnumber = 1
-                for item in value:
-                    points = numpy.array([item[0][0], item[0][1], item[0][2], item[0][3]],
-                                         dtype=numpy.float32)
+                    # add a way to track if it jumping or not
+                if index == 0:
+                    radian = 0
+                    if number[2] >= 0.8:
+                        radian = int(number[1]) + int((self.box_position[0] - 50) / self.pixel_per_mills)
+                        # print(f"Radian is: {radian}")
 
-                    boxheight = int(item[0][2][1] - item[0][0][1])
-                    print(f"box#{boxnumber} height: {boxheight}")
+            if debug:
+                if self.radian_results:
+                    boxnumber = 1
+                    for item in self.radian_results:
+                        points = numpy.array([item[0][0], item[0][1], item[0][2], item[0][3]],
+                                             dtype=numpy.float32)
+                        # Get the minimum area rectangle
+                        rect = cv2.minAreaRect(points)
 
-                    # Get the minimum area rectangle
-                    rect = cv2.minAreaRect(points)
+                        # Get the four corner points of the rectangle
+                        box = cv2.boxPoints(rect)
+                        box = numpy.intp(box)  # Convert the points to integer
+                        cv2.polylines(self.radian_monochrome, [box], isClosed=True, color=(0, 255, 0), thickness=2)
+                        cv2.imshow("Radian Boxed Monochrome Screen Capture", self.radian_monochrome)
+                        boxnumber += 1
 
-                    # Get the four corner points of the rectangle
-                    box = cv2.boxPoints(rect)
-                    box = numpy.intp(box)  # Convert the points to integer
-                    cv2.polylines(self.radian_monochrome, [box], isClosed=True, color=(0, 255, 0), thickness=2)
-                    cv2.imshow("Radian Boxed Monochrome Screen Capture", self.radian_monochrome)
-                    boxnumber += 1
-
-        return value
+                # this should return the approximate radian calculated from the pixel difference and the radian detected
+            if radian:
+                return radian
+            else:
+                return 0
 
     def get_bearing(self):
         return self.get_bearing_ocr_results()
+
 
 def calculate_fps(frame_count, fps_display_interval, last_time):
     if debug:
@@ -150,7 +174,7 @@ def calculate_fps(frame_count, fps_display_interval, last_time):
             ms_per_frame = (elapsed_time / frame_count) * 1000
             last_time = current_time
             frame_count = 0
-            print(f"FPS: {fps:.2f}, Frame Time: {ms_per_frame:.2f} ms")
+            # print(f"FPS: {fps:.2f}, Frame Time: {ms_per_frame:.2f} ms")
         return last_time, frame_count
 
 
