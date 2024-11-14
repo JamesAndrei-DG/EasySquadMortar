08593c6de6617b23b116ec30b462fa1cb6cf76fd
JamesAndrei-DG <jamesandrei.dg@gmail.com>
2024-11-14 23:08:16 +0800

    Updated .gitignore Added function to generate html for all maps

--- generatehtml.py
+++ generatehtml.py
@@ -3,6 +3,7 @@ import jinja2
 from folium.plugins import Realtime
 import re
 
+
 def parse_maps():
     with open('./maps/maps.js', 'r') as file:
         js_template = file.read()
@@ -23,11 +24,11 @@ def parse_maps():
 
     for i in range(len(name)):
         map_row = [
-            name[i],
-            int(size[i]),
-            float(scaling[i]),
-            mapURL[i],
-            int(maxZoomLevel[i])
+            name[i],  # 0
+            int(size[i]),  # 1
+            float(scaling[i]),  # 2
+            mapURL[i],  # 3
+            int(maxZoomLevel[i])  # 4
         ]
         maps.append(map_row)
 
@@ -36,76 +37,73 @@ def parse_maps():
 
 maps_array = parse_maps()
 
+for i, data in enumerate(maps_array):
+    # Create the map object
+    base_map = folium.Map(crs='Simple', zoom_start=4)
+    map_overlay = folium.raster_layers.ImageOverlay(
+        image=str("./maps" + data[3] + "basemap.webp"),
+        bounds=[[0, 0], [-int(data[1]), int(data[1])]],
+        zigzag_index=1
+    )
 
+    map_overlay.add_to(base_map)
+    base_map.fit_bounds(bounds=[[0, 0], [-int(data[1]), int(data[1])]])
 
-# Create the map object
-Base_Map = folium.Map(crs='Simple', zoom_start=4)
-map_overlay = folium.raster_layers.ImageOverlay(
-    image='albasrah.webp',
-    bounds=[[0,0], [-3040,3040]],
-    zigzag_index=1
-)
+    # Set name for map so we can use javascript to manipulate.
+    base_map._name = "map"
+    base_map._id = "1"
 
-albasrah_overlay.add_to(Base_Map)
-Base_Map.fit_bounds(bounds=[[0,0], [-3040,3040]])
-
-#Set name for map so we can use javascript to manipulate.
-Base_Map._name = "map"
-Base_Map._id = "1"
-
-
-#Add Custom Javascript
-el = folium.MacroElement().add_to(Base_Map)
-el._template = jinja2.Template("""
-    {% macro script(this, kwargs) %}
-    // write JS here
-    function ZoomOnUpdate(longitude, latitude) {
-    map_1.setView([latitude, longitude], 2);  // Set the center to the coordinates and zoom level to 12
-}
-    {% endmacro %}
-""")
-
-#custom javascript
-#https://stackoverflow.com/a/58802382
-
-# JavaScript for fetching satellite data and added zoomonmarker function call
-source = folium.JsCode("""
-    function(responseHandler, errorHandler) {
-        var url = 'https://api.wheretheiss.at/v1/satellites/25544';
-        
-        fetch(url)
-        .then((response) => {
-            return response.json().then((data) => {
-                var { id, longitude, latitude } = data;
-
-                return {
-                    'type': 'FeatureCollection',
-                    'features': [{
-                        'type': 'Feature',
-                        'geometry': {
-                            'type': 'Point',
-                            'coordinates': [longitude, latitude]
-                        },
-                        'properties': {
-                            'id': id
-                        }
-                    }]
-                };
-            })
-        })
-        .then((data) => {
-            responseHandler(data);
-            // Call the ZoomOnUpdate function here
-            ZoomOnUpdate(data.features[0].geometry.coordinates[0], data.features[0].geometry.coordinates[1]);
-        })
-        .catch(errorHandler);
+    # Add Custom Javascript
+    el = folium.MacroElement().add_to(base_map)
+    el._template = jinja2.Template("""
+        {% macro script(this, kwargs) %}
+        // write JS here
+        function ZoomOnUpdate(longitude, latitude) {
+        map_1.setView([latitude, longitude], 2);  // Set the center to the coordinates and zoom level to 12
     }
-""")
+        {% endmacro %}
+    """)
 
-# Add Realtime plugin to the map
-rt = Realtime(source, interval=10000)
-rt.add_to(Base_Map)
+    # custom javascript
+    # https://stackoverflow.com/a/58802382
 
+    # JavaScript for fetching satellite data and added zoomonmarker function call
+    source = folium.JsCode("""
+        function(responseHandler, errorHandler) {
+            var url = 'https://api.wheretheiss.at/v1/satellites/25544';
+            
+            fetch(url)
+            .then((response) => {
+                return response.json().then((data) => {
+                    var { id, longitude, latitude } = data;
+    
+                    return {
+                        'type': 'FeatureCollection',
+                        'features': [{
+                            'type': 'Feature',
+                            'geometry': {
+                                'type': 'Point',
+                                'coordinates': [longitude, latitude]
+                            },
+                            'properties': {
+                                'id': id
+                            }
+                        }]
+                    };
+                })
+            })
+            .then((data) => {
+                responseHandler(data);
+                // Call the ZoomOnUpdate function here
+                ZoomOnUpdate(data.features[0].geometry.coordinates[0], data.features[0].geometry.coordinates[1]);
+            })
+            .catch(errorHandler);
+        }
+    """)
 
-# Save the map as an HTML file
-Base_Map.save("qt/screen/test.html")
+    # Add Realtime plugin to the map
+    rt = Realtime(source, interval=10000)
+    rt.add_to(base_map)
+
+    # Save the map as an HTML file
+    base_map.save(str("qt/screen/maps/" + data[0] + ".html"))
