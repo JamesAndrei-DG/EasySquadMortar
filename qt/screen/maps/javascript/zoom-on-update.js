0dc63f7b997a2cad029c6e48ba6d60cee09e0425
JamesAndrei-DG <jamesandrei.dg@gmail.com>
2024-11-17 21:47:11 +0800

    fix: Fixed zoom-on-update.js, changed it to use latlng leaflet

--- qt/screen/maps/javascript/zoom-on-update.js
+++ qt/screen/maps/javascript/zoom-on-update.js
@@ -1,3 +1,3 @@
-function ZoomOnUpdate(longitude, latitude) {
-    map_1.setView([latitude, longitude], 2);  // Set the center to the coordinates and zoom level to 12
+function ZoomOnUpdate(latlng ) {
+    map_1.setView(latlng , 6);  // Set the center to the coordinates and zoom level to 12
 }
