9376e2f442a5b815fca4d18e522a1fa57a512cc6
JamesAndrei-DG <jamesandrei.dg@gmail.com>
2024-11-18 19:49:33 +0800

    refactor: refactored parsing maps to an array

--- generatehtml.py
+++ generatehtml.py
@@ -1,41 +1,9 @@
 import folium
 import jinja2
 from folium.plugins import Realtime
-import re
+from tools import parse_maps
 
-
-def parse_maps():
-    with open('./maps/maps.js', 'r') as file:
-        js_template = file.read()
-
-    name_pattern = r'name:\s*"([^"]*)"'
-    size_pattern = r'size:\s*(\d+)'
-    scaling_pattern = r'scaling:\s*([\d\.]+)'
-    mapURL_pattern = r'mapURL:\s*"([^"]*)"'
-    maxZoomLevel_pattern = r'maxZoomLevel:\s*(\d+)'
-
-    maps = []
-
-    name = re.findall(name_pattern, js_template)
-    size = re.findall(size_pattern, js_template)
-    scaling = re.findall(scaling_pattern, js_template)
-    mapURL = re.findall(mapURL_pattern, js_template)
-    maxZoomLevel = re.findall(maxZoomLevel_pattern, js_template)
-
-    for i in range(len(name)):
-        map_row = [
-            name[i],  # 0
-            int(size[i]),  # 1
-            float(scaling[i]),  # 2
-            mapURL[i],  # 3
-            int(maxZoomLevel[i])  # 4
-        ]
-        maps.append(map_row)
-
-    return maps
-
-
-maps_array = parse_maps()
+maps_array = parse_maps.parse()
 
 for i, data in enumerate(maps_array):
     base_map = folium.Map(crs='Simple', zoom_start=4)
