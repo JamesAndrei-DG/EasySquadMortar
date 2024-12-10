6dc722639ef317deec3c3992ce5cd3a6d5b1564f
JamesAndrei-DG <jamesandrei.dg@gmail.com>
2024-12-10 22:43:25 +0800

    fix: map function now correctly calculates the location due to y-axis calculated incorrectly

--- core/map_Qobject.py
+++ core/map_Qobject.py
@@ -33,21 +33,21 @@ class Maps(QObject):
     @Slot(str)
     def mortar_position(self, keypad: str):
         print(f"Origin is: {keypad}")
-        # thread = threading.Thread(target=self.MapFunction.set_origin_keypad, args=(keypad,))
-        # thread.start()
+        thread = threading.Thread(target=self.MapFunction.set_origin_keypad, args=(keypad,))
+        thread.start()
 
-        self.MapFunction.set_origin_keypad(keypad)
+        # self.MapFunction.set_origin_keypad(keypad)
 
     # For Testing
     @Slot(str)
     def target_position(self, keypad: str):
-        # self.target_keypad = keypad
-        # print(f"Target is: {keypad}")
-        # value = self.MapFunction.shoot_distance(83,1470)
+
+        value = self.MapFunction.shoot_distance(83,1480)
+        # print(value)
         # print("Location printing: I10-5-8-5-7")
         # test = self.MapFunction.get_keypad_position("I10-5-8-5-7")
         # print(f"{test}")
 
         # test2 = self.MapFunction.get_distance(1470* 0.981719 * 0.001,83)
         # print(f"Manual Fire \n{test2}")
-        self.MapFunction.test_me()
+        # self.MapFunction.test_me()
--- core/map_functions.py
+++ core/map_functions.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 from time import perf_counter
+import inspect
 
 
 class MapFunction:
@@ -13,7 +14,10 @@ class MapFunction:
         self.array_heightlines = []
 
         self.origin_x = 0
-        self.origin_y = 0  # should be zero
+        self.origin_y = 0
+
+        # bool
+        self.precalculated = False
 
     def change_map(self, array_number):
         print(f"Changing array map")
@@ -31,7 +35,7 @@ class MapFunction:
         print(f"x: {self.origin_x} y: {self.origin_y}")
         self._precalculate_fire_solution()
 
-    # Need to sanitize before using get_position
+    # Need to sanitize before using function
     def get_keypad_position(self, keypad):
         try:
             keys = keypad.split("-")
@@ -61,12 +65,26 @@ class MapFunction:
             y += interval / 2
             return int(x), int(y)
         except Exception as error:
-            print(f"Error Encountered in get_keypad_position\n{error}")
+            print(f"Error Encountered in {inspect.currentframe().f_code.co_name}\n{error}")
+
+    def xy_to_keypad(self):
+        pass
 
     def get_height(self, x, y):
-        return self.array_map_height[x][y]
+        # is there better way to fix this?
+        # if x > 0 or y > 0 :
+        #     raise ValueError(f"x:{x} y:{y} is invalid")
+        # elif x > size or y > size:
+        #     raise ValueError(f"x:{x} y:{y} is invalid")
+        try:
+            return self.array_map_height[x][y]
+        except IndexError:
+            print(f"x:{x} y:{y} is out of bounds")
+            return 0
+        except Exception as error:
+            raise Exception(f"Error Encountered in {inspect.currentframe().f_code.co_name}\n{error}")
 
-    def get_distance(self, rad, height_array):
+    def get_distance(self, rad, elevation_array):
         origin_elevation = self.get_height(self.origin_x, self.origin_y)
         x = 0
         y = 0
@@ -90,7 +108,7 @@ class MapFunction:
         vx = velocity * math.cos(rad)
         vy = velocity * math.sin(rad)
         step = 0.01
-        height_arr = height_array
+        height_arr = elevation_array
         try:
             while True:
                 # Update position
@@ -107,17 +125,17 @@ class MapFunction:
                 # Increment time
                 t += step
         except Exception as error:
-            print(f"Error Encountered in get_distance\n{error}")
+            print(f"Error Encountered in {inspect.currentframe().f_code.co_name}\n{error}")
 
-    def get_height_range_from_azimuth(self, azimuth):
+    def _calculate_all_possible_distances_from_azimuth(self, azimuth):
         result = []
-        height_array = self._get_height_line_array(azimuth)
+        elevation_range = self._calculate_elevation_range_from_azimuth(azimuth)
         for natomil in range(800, 1580 + 1, 10):
-            result.append(self.get_distance((natomil * 0.981719 * 0.001), height_array))
+            result.append(self.get_distance((natomil * 0.981719 * 0.001), elevation_range))  # natomil to rad
 
         return result
 
-    def _get_height_line_array(self, azimuth):
+    def _calculate_elevation_range_from_azimuth(self, azimuth):
         rad = azimuth * math.pi / 180
         # find height 0-1500 meters out with 10 meters step lets check later if i can make it more resolution
         x_scale = np.sin(rad)
@@ -132,32 +150,42 @@ class MapFunction:
         return height_array
 
     def _precalculate_fire_solution(self):
+        self.precalculated = False
         print("Precalculating fire solution")
         t1 = perf_counter()
         for azimuth in range(359):
-            self.array_heightlines.append(self.get_height_range_from_azimuth(azimuth))
+            self.array_heightlines.append(self._calculate_all_possible_distances_from_azimuth(azimuth))
         t2 = perf_counter()
         time = (t2 - t1) * 1000
         print(f"Calculation Finished in {time} ms")
+        self.precalculated = True
 
     def shoot_distance(self, azimuth, natomils):
+        # Check Exceptions
         if natomils < 800 or natomils > 1580:
             raise ValueError(f"{natomils} is an invalid input in natomils")
         elif azimuth < 0 or azimuth > 359:
             raise ValueError(f"{azimuth} is an invalid input in azimuth")
 
-        index = int((natomils - 800) / 10)
+        # If not precalculated
+        if self.precalculated == False:
+            # approximation
+            elevation_array = self._calculate_elevation_range_from_azimuth(int(azimuth))
+            return self.get_distance(natomils * 0.981719 * 0.001, elevation_array)
+
+        index = max(0, int((natomils - 800) / 10))
+        bearing = int(azimuth)
         try:
-            value = self.array_heightlines[azimuth][index]
+            value = self.array_heightlines[bearing][index]
             print("Shoot success")
             print(value)
             return value
         except Exception as error:
-            print(f"Error Encountered in shoot_distance\n{error}")
+            print(f"Error Encountered in {inspect.currentframe().f_code.co_name}\n{error}")
 
     def test_me(self):
         print("testing")
-        array = self._get_height_line_array(83)
+        array = self._calculate_elevation_range_from_azimuth(83)
         print("target height at 300m")
         value = array[30]
         print(value)
