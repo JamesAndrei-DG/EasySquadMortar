1b1648999c625b932e13a462dd0b4fff46425217
JamesAndrei-DG <jamesandrei.dg@gmail.com>
2024-12-14 01:35:13 +0800

    Revert "feat: added multiprocessing for easyocr"
    This reverts commit 0e4685245ffc67e8c2267c46407e5b2886b7f707.

--- core/object_workers.py
+++ core/object_workers.py
@@ -1,6 +1,21 @@
 from PySide6.QtCore import QObject
+from core.parse_screen import ParseScreen
 from core.fastapi_sse import app
 import uvicorn
+
+
+class ObjectEasyOCR(QObject):
+    """Class responsible for running EasyOCR in a loop."""
+
+    def run_easyocr(self) -> None:
+        """Runs the EasyOCR process."""
+        easyocr = ParseScreen()
+        while True:
+            NatoMil = easyocr.get_natomil()
+            Azimuth = easyocr.get_azimuth()
+
+            # print(f"NatoMil: {easyocr.get_natomil()}")
+            # print(f"Azimuth: {easyocr.get_azimuth()}")
 
 
 class ObjectFastApi(QObject):
@@ -8,4 +23,4 @@ class ObjectFastApi(QObject):
 
     def run_fast_api_server(self) -> None:
         """Runs the FastAPI server."""
-        uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)  # might need to change the app to a string
+        uvicorn.run(app, host="127.0.0.1", port=8000, reload=False) #might need to change the app to a string
